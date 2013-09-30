74942a0c3549a07db44eb0b77fc6a05a79040da0
Ricardo Rauch <rickyrauch@gmail.com>
2013-09-30 01:37:04 -0300

    fixes css

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -35,25 +35,23 @@ function symbol_content_nav( $nav_id ) {
 		$nav_class = 'site-navigation post-navigation';
 
 	?>
-	<nav role="navigation" id="<?php echo $nav_id; ?>" class="center-content <?php echo $nav_class; ?>">
-		<div class="article-content">
-	<?php if ( is_single() ) : // navigation links for single posts ?>
+		<nav role="navigation" id="<?php echo $nav_id; ?>" class="center-content <?php echo $nav_class; ?>">
+		<?php if ( is_single() ) : // navigation links for single posts ?>
 
-		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'symbol' ) . '</span> %title' ); ?>
-		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'symbol' ) . '</span>' ); ?>
+			<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'symbol' ) . '</span> %title' ); ?>
+			<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'symbol' ) . '</span>' ); ?>
 
-	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+		<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
-		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'symbol' ) ); ?></div>
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'symbol' ) ); ?></div>
+			<?php endif; ?>
+
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'symbol' ) ); ?></div>
+			<?php endif; ?>
+
 		<?php endif; ?>
-
-		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'symbol' ) ); ?></div>
-		<?php endif; ?>
-
-	<?php endif; ?>
-		</div>
 	</nav><!-- #<?php echo $nav_id; ?> -->
 	<?php
 }
